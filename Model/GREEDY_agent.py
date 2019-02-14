d640959f07036153f67ce2b455f02fb018966681
gordon0803 <qian39@purdue.edu>
2019-02-13 19:43:40 -0500

    Add files via upload

--- Model/GREEDY_agent.py
+++ Model/GREEDY_agent.py
@@ -13,16 +13,17 @@ class greedy_agent():
         #config is the parameter setting
         #ckpt_path is the path for load models
         self.name=name
+        self.N_station = N_station
+        self.N_station_pair = N_station*N_station
 
     def predict(self,s):
         #make the prediction
-        passenger_gap = np.diag(np.reshape(s[range(0,300,3)],(10,10)))
+        passenger_gap = np.diag(np.reshape(s[range(0,len(s),len(s)//self.N_station_pair)],(self.N_station,self.N_station)))
         action=np.argmax(passenger_gap)
         return action
 
     def predict_softmax(self,s):
-        passenger_gap = np.diag(np.reshape(s[range(0,300,3)],(10,10)))
+        passenger_gap = np.diag(np.reshape(s[range(0,len(s),len(s)//self.N_station_pair)],(self.N_station,self.N_station)))
         utility = np.exp(np.array(passenger_gap))
         prob = utility / sum(utility)
         return prob
-
