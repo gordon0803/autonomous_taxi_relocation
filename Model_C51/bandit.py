1196fd92a75e053921097d5258f19623ea79f8bb
gordon0803 <qian39@purdue.edu>
2019-03-08 09:33:02 -0500

    Add files via upload

--- Model_C51/bandit.py
+++ Model_C51/bandit.py
@@ -1,4 +1,18 @@
 import numpy as np
+import time
+import numba as nb
+
+
+# @nb.jit
+# def _return_upper_bound_batch(features,theta,alpha,AaI,n_action):
+#     prob=[0 for i in range(len(features))]
+#     count=0;
+#     for feature in features:
+#         tprob=[feature.dot(theta[i])+alpha*np.sqrt((feature.dot(AaI[i])).dot(feature)) for i in range(n_action)]
+#         prob[count]=np.array(tprob)
+#         count+=1
+#     return np.array(prob)
+
 
 class linucb_agnet():
     def __init__(self,n_action,d):
@@ -7,19 +21,19 @@ class linucb_agnet():
         self.alpha=1
         self.d=d
         self.n_action=n_action
-        self.Aa={} #collection of A for each arm
-        self.ba={} #collection of vectors to compute disjoint part d*1
-        self.AaI={} #inverse of A
-        self.Da={}
-        self.theta={}
+        self.Aa=[] #collection of A for each arm
+        self.ba=[] #collection of vectors to compute disjoint part d*1
+        self.AaI=[] #inverse of A
+        self.Da=[]
+        self.theta=[]
 
         #initialize parameters
         for i in range(n_action):
-            self.Aa[i]=np.identity(self.d)
-            self.ba[i]=np.zeros(self.d)
-            self.AaI[i]=np.identity(self.d)
-            self.Da[i]=np.zeros((self.d,self.d))
-            self.theta[i]=np.zeros(self.d)
+            self.Aa.append(np.identity(self.d))
+            self.ba.append(np.zeros(self.d))
+            self.AaI.append(np.identity(self.d))
+            self.Da.append(np.zeros((self.d,self.d)))
+            self.theta.append(np.zeros(self.d))
 
     def update(self,features,actions,rewards):
         #update all observed arms
@@ -44,17 +58,23 @@ class linucb_agnet():
         #done update
 
     def return_upper_bound(self,feature):
-        prob=[]
+        # prob=[]
         s=np.array(feature)
-        for i in range(self.n_action):
-            score=np.dot(s,self.theta[i])+self.alpha*np.sqrt(np.dot(np.dot(s,self.AaI[i]),s))
-            prob.append(score)
+        #
+        # for i in range(self.n_action):
+        #     score=np.dot(s,self.theta[i])+self.alpha*np.sqrt(np.dot(np.dot(s,self.AaI[i]),s))
+        #     prob.append(score)
+
+        # prob=[np.dot(s,self.theta[i])+self.alpha*np.sqrt(np.dot(np.dot(s,self.AaI[i]),s)) for i in range(self.n_action)]
+
+        prob=np.fromiter((np.dot(s,self.theta[i])+self.alpha*np.sqrt(np.dot(np.dot(s,self.AaI[i]),s)) for i in range(self.n_action)), float)
+        # prob=_return_upper_bound(s,self.theta,self.alpha,self.AaI,self.n_action)
+
         return np.array(prob)
 
     def return_upper_bound_batch(self,feature):
-        prob=[]
-        for i in range(len(feature)):
-            prob.append(self.return_upper_bound(feature[i]))
+        t1=time.time()
+        prob=[self.return_upper_bound(feature[i]) for i in range(len(feature))]
 
         return np.array(prob)
 
