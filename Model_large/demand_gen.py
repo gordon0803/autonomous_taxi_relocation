4440cc18f859e6de5835ca69e98133672023e342
gordon0803 <qian39@purdue.edu>
2019-04-20 17:58:55 -0400

    Update demand_gen.py

--- Model_large/demand_gen.py
+++ Model_large/demand_gen.py
@@ -13,19 +13,17 @@ travel_time = np.loadtxt(open('nycdata/selected_time.csv','rb'),delimiter=',')
 
 #load the list of OD files, and normalize them to proper time interval
 OD_mat=[]
-normalize_od=30;
+normalize_od=60;
 for file in glob.glob('od_mat/*.csv'):
     print(file)
     tempOD=np.genfromtxt(file, delimiter=',')
-    tempOD/=mormalized_od #convert into every minutes 
+    tempOD/=normalized_od #convert into every 30 seconds
     OD_mat.append()
     
 #OD_mat=np.loadtxt(open('nycdata/od_70.csv','rb'),delimiter=',')
 
-travel_time=travel_time;
-
 #convert arrival rate into 
-travel_time=travel_time;
+travel_time=travel_time*2;
 
 #process arrival input, each item is the 48 time intervals for each station
 arrival_rate=[[] for i in range(N_station)] #initialize
@@ -33,19 +31,17 @@ for i in range(len(OD_mat)):
     demand=OD_mat.sum(axis=1) #row sum
     for j in range(len(demand)):
         arrival_rate[j].append(demand[j])
+        
+OD_mat=[i.tolist() for i in OD_mat]
 
-arrival_rate=OD_mat.sum(axis=1) #row sum for passenger arrival at the station
-incoming__taxi=OD_mat.sum(axis=0)
-
-OD_mat=OD_mat.tolist()
-
-exp_dist=[] #expected trip distance starting at each station
-for i in range(N_station):
-    v=0
-    for j in range(N_station):
-        v+=distance[i,j]*OD_mat[i][j]
-
-    exp_dist.append(v/sum(OD_mat[i]))
+exp_dist=[[] for i in range(len(OD_mat)] #expected trip distance starting at each station for each time interval
+                            
+for t in range(len(OD_mat)):
+    for i in range(N_station):
+        v=0
+        for j in range(N_station):
+            v+=distance[i,j]*OD_mat[t][i][j]
+        exp_dist[t].append(v/sum(OD_mat[i]))
 
 taxi_input = 40
 
@@ -57,10 +53,6 @@ simulation_input['taxi_input'] = taxi_input
 simulation_input['OD_mat'] = OD_mat
 simulation_input['arrival_rate'] = arrival_rate
 simulation_input['exp_dist']=exp_dist
-
-#relo_graph = taxi_util.RGraph(distance, incoming_taxi, arrival_rate)
-# relo_graph = taxi_util.RGraph_reward(travel_time, np.array(OD_mat), arrival_rate)
-# simulation_input['RG']=relo_graph
-
+                            
 with open('simulation_input.dat', 'wb') as fp:
     pickle.dump(simulation_input, fp)
